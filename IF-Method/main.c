9b7f1f02f828cb2e155f79c1a95b32e96f099b92
yogendra singh <yogendra20799@gmail.com>
2018-11-24 16:53:03 +0530

    Initial Commit

---
+++ IF-Method/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IF-Method
+//
+//  Created by yogendra singh on 11/24/18.
+//  Copyright © 2018 yadavboy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
